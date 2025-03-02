80ad07812a6588362394b71c7bc2f65272e8882c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-02 22:18:23 +0900

    add: tailwind setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // すべてのJS、JSX、TS、TSXファイルをスキャン
+  ],
+  future: {
+  },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "aqua", "pastel"],
+  },
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
