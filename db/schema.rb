968386982adf20ce3c8282cd13799ef5e9ca3d1c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-05 00:54:12 +0900

    add: Usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_084432) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_155330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_084432) do
     t.string "portfolio_link"
     t.string "x_id"
     t.boolean "attend?", default: false, null: false
+    t.integer "term"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
