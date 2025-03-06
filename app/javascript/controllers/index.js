2f2e9738586334212b2860e8d9eb8d7f770ead7f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-06 14:42:27 +0900

    add: stimulus modalインストール

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,8 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { Application } from '@hotwired/stimulus'
+import Dialog from '@stimulus-components/dialog'
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+const application = Application.start()
+application.register('dialog', Dialog)
