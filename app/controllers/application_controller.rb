23beea924a39e6b33d0152bef16e034f618f8c60
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-07 22:05:01 +0900

    fix: リダイレクト先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  private
+
+  def after_sign_up_path_for(resource_or_scope)
+    attendees_path
+  end
+
+  def after_update_path_for(resource_or_scope)
+    attendees_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    attendees_path
+  end
+
   protected
 
   def configure_permitted_parameters
