9fbcf789f8344bbcdc0d185f175cfec0e304b5b3
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-07 16:12:08 +0900

    add: 表示をアップデート順に修正

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,5 +1,5 @@
 class AttendeesController < ApplicationController
   def index
-    @attendees = User.where(attend?: true)
+    @attendees = User.where(attend?: true).order(updated_at: :desc)
   end
 end
