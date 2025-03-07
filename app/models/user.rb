905fdc8533fe5208cdbefb42b8dfbeb378a4ffaa
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-07 21:24:34 +0900

    add: validates関連追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   enum :role, { student: 0, ex_student: 1, operator: 2 }
 
+  validates :password, presence: true, on: :create
+  validates :username, presence: true
+  validates :email, presence: true
+
   def x_profile_link
     "https://x.com/#{x_id}"
   end
@@ -26,5 +30,19 @@ class User < ApplicationRecord
 
   def human_attribute_enum(attr_name)
     self.class.human_attribute_enum_value(attr_name, self[attr_name])
+  end
+
+  def update_with_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank?
+        params.delete(:password)
+        params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update(params, *options)
+
+    clean_up_passwords
+    result
   end
 end
