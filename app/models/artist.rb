197f27cc0e4fe83d7956e99f322728ac6aa37b69
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-09 00:14:13 +0900

    fix: スペル修正

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ApplicationRecord
   validates :performance_category, presence: true
   validates :requested_play_time, presence: true
 
-  enum performance_category: { band: 0, dance: 1, accostic: 2, session: 3, instrumental: 4, other: 5 }
+  enum performance_category: { band: 0, dance: 1, acoustic: 2, session: 3, instrumental: 4, other: 5 }
 
   has_many :artist_members, dependent: :destroy
   has_many :users, through: :artist_members
