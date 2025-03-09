ede3e08143a080e8a1cdcf0a63ed74817550dc3c
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-09 09:32:07 +0900

    add:レコード作成処理記述

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,5 +10,18 @@ class ArtistsController < ApplicationController
   end
 
   def create
+    @artist = Artist.new(artist_params)
+    p @artist
+    if @artist.save
+      redirect_to artists_path, notice: 'アーティストを登録しました'
+    else
+      render :new, alert: 'アーティストの登録に失敗しました'
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :performance_category, :requested_play_time, :performance_content, artist_members_attributes: [:id, :user_id, :_destroy])
   end
 end
