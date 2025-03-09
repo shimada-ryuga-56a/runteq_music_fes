03805ea3f5088225819e81e681c7cb3fd49c46fc
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-09 09:30:38 +0900

    add: 演奏時間のenum値追加

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,7 @@ class Artist < ApplicationRecord
   validates :requested_play_time, presence: true
 
   enum performance_category: { band: 0, dance: 1, acoustic: 2, session: 3, instrumental: 4, other: 5 }
+  enum requested_play_time: { five_minutes: 0, ten_minutes: 1, fifteen_minutes: 2, twenty_minutes: 3, twenty_five_minutes: 4 }
 
   has_many :artist_members, dependent: :destroy
   accepts_nested_attributes_for :artist_members, reject_if: :all_blank, allow_destroy: true
