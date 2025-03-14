7c6de6d9916d07230ca1a8d325c75e4eebc3dc36
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-14 21:42:11 +0900

    add: アーティストシステムスペック 仮記述

---
+++ spec/system/artists_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Artists", type: :system do
+  before do
+    driven_by(:selenium_chrome_headless)
+    @user = FactoryBot.create(:user)
+  end
+
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
+  describe '未ログイン時', js: true do
+    context "artists/indexにアクセスしたとき" do
+      it "一覧ページが表示される" do
+        visit artists_path
+        expect(page).to have_content '【演奏者一覧】'
+      end
+    end
+
+    context "artists/newにアクセスしたとき" do
+      it "ログイン画面にリダイレクトされる" do
+        visit new_artist_path
+        expect(page).to have_content 'ログイン'
+      end
+    end
+  end
+
+  describe 'ログイン時', js: true do
+    before do
+      sign_in @user
+    end
+
+    context "artists/indexにアクセスしたとき" do
+      it "一覧ページが表示される" do
+        visit artists_path
+        expect(page).to have_content '【演奏者一覧】'
+      end
+    end
+
+    context "artists/newにアクセスしたとき" do
+      it "新規登録画面が表示される" do
+        visit new_artist_path
+        expect(page).to have_content '出演希望申請フォーム'
+      end
+    end
+
+    context "artists/newでフォームを入力後" do
+      it "アーティストの新規登録ができる" do
+        visit new_artist_path
+        # fill_in "artist_name", with: 'artist_name'
+        # select 'バンド', from: 'artist_performance_category'
+        # select '5分', from: 'artist_requested_play_time'
+        # click_button '＋メンバー追加'
+        fill_in 'artist_performance_content', with: 'performance_content'
+        click_button '送信'
+        expect(current_path).to eq(artists_path)
+        expect(page).to have_content '出演登録が完了しました'
+      end
+    end
+  end
+end
