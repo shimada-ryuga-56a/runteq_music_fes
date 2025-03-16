e6d09f5f28487a17ac6f75615a453e982fa7b34d
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-16 23:39:24 +0900

    fix: Rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,14 +23,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update,
-                                      keys: [
-                                        :username,
-                                        :mattermost_link,
-                                        :portfolio_link,
-                                        :x_id,
-                                        :attend?,
-                                        :term,
-                                        :role
+                                      keys: %i[
+                                        username
+                                        mattermost_link
+                                        portfolio_link
+                                        x_id
+                                        attend?
+                                        term
+                                        role
                                       ])
   end
 end
--- db/migrate/20250316125715_remove_hope_from_playable_instruments.rb
+++ db/migrate/20250316125715_remove_hope_from_playable_instruments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveHopeFromPlayableInstruments < ActiveRecord::Migration[7.1]
   def change
     remove_column :playable_instruments, :hope_for_matching, :boolean
