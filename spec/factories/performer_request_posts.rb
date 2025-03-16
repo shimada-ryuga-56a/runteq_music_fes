162f6fec509a2f77673731ec87c7899184e8a5c6
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-17 00:53:25 +0900

    add: テスト作成

--- spec/factories/performer_request_posts.rb
+++ spec/factories/performer_request_posts.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :performer_request_post do
-    offer_or_request { 1 }
-    user_id { 1 }
-    comment { "MyText" }
+    offer_or_request { 0 }
+    comment { 'MyText' }
+    user
+  end
+
+  factory :correct_performer_request_post, class: PerformerRequestPost do
+    offer_or_request { 0 }
+    comment { 'MyText' }
+    user
   end
 end
--- spec/models/performer_request_post_spec.rb
+++ spec/models/performer_request_post_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PerformerRequestPost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    context 'when all attributes are present' do
+      it 'is valid' do
+        post = build(:correct_performer_request_post)
+        expect(post).to be_valid
+      end
+    end
+
+    context 'when offer_or_request is missing' do
+      it { is_expected.to validate_presence_of :offer_or_request }
+    end
+
+    context 'when comment is missing' do
+      it { is_expected.to validate_presence_of :comment }
+    end
+  end
 end
