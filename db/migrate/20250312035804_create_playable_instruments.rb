f8fa91e20dfd7023ccbe452dfffc27b8b04b92f4
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-17 10:43:38 +0900

    add: 誤って削除してしまったマイグレーションファイルを復元

---
+++ db/migrate/20250312035804_create_playable_instruments.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreatePlayableInstruments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :playable_instruments do |t|
+      t.string :instrument_name, null: false
+      t.integer :user_id, null: false
+      t.boolean :hope_for_matching?, null: false, default: true
+
+      t.timestamps
+    end
+  end
+end
