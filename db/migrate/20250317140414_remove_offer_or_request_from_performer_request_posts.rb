d3304cdf4d8b302f096f002c9d5f189784d116cf
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-18 00:14:23 +0900

    fix: Rubocop修正

--- db/migrate/20250317140414_remove_offer_or_request_from_performer_request_posts.rb
+++ db/migrate/20250317140414_remove_offer_or_request_from_performer_request_posts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveOfferOrRequestFromPerformerRequestPosts < ActiveRecord::Migration[7.1]
   def change
     remove_column :performer_request_posts, :offer_or_request, :integer
--- db/migrate/20250317140447_add_titles_to_performer_request_posts.rb
+++ db/migrate/20250317140447_add_titles_to_performer_request_posts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTitlesToPerformerRequestPosts < ActiveRecord::Migration[7.1]
   def change
     add_column :performer_request_posts, :title, :string, null: false, default: 'title'
