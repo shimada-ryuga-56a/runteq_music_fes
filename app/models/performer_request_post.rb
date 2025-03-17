86a19a0f9e06ce5a9af4a3815ef991569fbfc101
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-18 00:05:51 +0900

    fix: バリデーション修正

--- app/models/performer_request_post.rb
+++ app/models/performer_request_post.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class PerformerRequestPost < ApplicationRecord
-  validates :offer_or_request, presence: true
+  validates :title, presence: true
   validates :comment, presence: true
 
   belongs_to :user
-
-  enum offer_or_request: { offer: 0, request: 1 }
 end
