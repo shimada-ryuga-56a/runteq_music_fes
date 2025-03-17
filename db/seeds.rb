63919c82f7b485df6a02833adfc0a189fad0d062
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-18 00:20:57 +0900

    fix: seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+@artists = ["あいみょん", "BUMP OF CHICKEN", "King Gnu", "Mrs. GREEN APPLE", "Official髭男dism", "Perfume", "RADWIMPS", "Reol", "米津玄師", "YOASOBI". "ヨルシカ", "和楽器バンド"]
+
 # ユーザー
 puts '== Creating Users =='
 
@@ -26,13 +28,20 @@ puts '== Creating Users =='
   end
 end
 
+puts '== Deleting No Title PerformerRequestPosts =='
+@no_title_performer_request_posts = PerformerRequestPost.where(title: "title")
+@no_title_performer_request_posts.each do |post|
+  puts '*'
+  post.destroy
+end
+
 # 出演募集 掲示板
 puts '== Creating PerformerRequestPosts =='
 10.times do |i|
   PerformerRequestPost.find_or_create_by!(comment: "comment#{i}", title: "title#{i}") do |post|
     puts '*'
-    post.title = "title#{i}"
+    post.title = "募集タイトル#{i}"
     post.user_id = 1
-    post.comment = "comment#{i}"
+    post.comment = "#{@artists[i]}の、〇〇のパートを募集しています。"
   end
 end
