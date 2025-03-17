5967200011a87a22a10ac2d8f3f766f0202f7d70
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-17 19:22:29 +0900

    add: 編集ページ用テスト作成

--- spec/system/performer_request_posts_spec.rb
+++ spec/system/performer_request_posts_spec.rb
@@ -56,5 +56,22 @@ RSpec.describe 'PerformerRequestPosts', type: :system do
         expect(page).to have_content '出演希望者掲示板'
       end
     end
+
+    context "自分の投稿の編集画面にアクセスしたとき" do
+      it '編集画面が表示される' do
+        post = @user.performer_request_posts.create(offer_or_request: 'offer', comment: 'comment')
+        visit edit_performer_request_post_path(post)
+        expect(page).to have_content '出演希望者掲示板 投稿編集'
+      end
+
+      it '編集ができる' do
+        post = @user.performer_request_posts.create(offer_or_request: 'offer', comment: 'comment')
+        visit edit_performer_request_post_path(post)
+        fill_in 'performer_request_post_comment', with: 'comment2'
+        click_button '更新する'
+        expect(current_path).to eq(performer_request_posts_path)
+        expect(page).to have_content '編集が完了しました'
+      end
+    end
   end
 end
