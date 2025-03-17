0d151e96c3f3e113e681b80303b25568ad8db7c2
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-17 17:26:42 +0900

    add: seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# ユーザー
+puts "== Creating Users =="
+
+10.times do |i|
+  User.find_or_create_by!(email: "example#{i}@email.com") do |user|
+    puts "*"
+    user.username = "example#{i}"
+    user.email = "example#{i}@email.com"
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.role = i % 3
+    user.term = i * 3
+  end
+end
+
+# 出演希望者掲示板
+puts "== Creating PerformerRequestPosts =="
+10.times do |i|
+  PerformerRequestPost.find_or_create_by!(comment: "comment#{i}") do |post|
+    puts "*"
+    post.offer_or_request = i % 2
+    post.user_id = 1
+    post.comment = "comment#{i}"
+  end
+end
